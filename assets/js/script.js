324950ac112c3262f7ddf5f2e5805be4a5ac2b5a
StellaKarolinaNunes <stellakarolina.peixoto@gmail.com>
2024-03-09 18:07:49 -0300

    atualização primeira pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,17 @@
-function fuja() {
-    const naoButton = document.getElementById("nao-button");
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const simBtn = document.querySelector('.sim-btn');
+const naoBtn = document.querySelector('.nao-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const naoBtnRect = naoBtn.getBoundingClientRect();
 
-    // Defina as coordenadas do quadrado desejado
-    const squareX = 100; // Ajuste conforme necessário
-    const squareY = 100; // Ajuste conforme necessário
-    const squareSize = 300; // Ajuste conforme necessário
+simBtn.addEventListener('click', () => {
+  window.location.href = 'teamo.html'; 
+});
 
-    const minX = squareX;
-    const minY = squareY;
-    const maxX = squareX + squareSize - naoButton.offsetWidth;
-    const maxY = squareY + squareSize - naoButton.offsetHeight;
-
-    const randomX = Math.floor(Math.random() * (maxX - minX + 1)) + minX;
-    const randomY = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
-
-    naoButton.style.left = randomX + "px";
-    naoButton.style.top = randomY + "px";
-}
-
-const naoButton = document.getElementById("nao-button");
-naoButton.addEventListener("mouseover", fuja);
-// script.js
-
-const simButton = document.querySelector('.sim');
-
-simButton.addEventListener('click', () => {
-
-    window.location.href = 'obrigado.html';
+naoBtn.addEventListener('mouseover', () => {
+  const i = Math.floor(Math.random() * (wrapperRect.width - naoBtnRect.width)) + 1;
+  const j = Math.floor(Math.random() * (wrapperRect.height - naoBtnRect.height)) + 1;
+  naoBtn.style.left = i + 'px';
+  naoBtn.style.top = j + 'px';
 });
